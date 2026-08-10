1f4082bcaa1f5e28228ff870e209c58b3b10ccdc
jryba94-maker <jryba.94@gmail.com>
2026-08-10 08:43:12 +0200

    Build minimal Fiszy app shell

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Fiszy</h1>
+      <p>MVP w budowie</p>
+      <p>Pierwsza prawdziwa aukcja powstaje tutaj.</p>
+    </main>
+  );
+}
